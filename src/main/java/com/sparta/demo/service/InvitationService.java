98062b4f94b99f286971cfb90632b450c856d2a7
insu <insubono@gmail.com>
2023-08-10 16:11:59 +0900

    <service> 38, 58번째줄 throw -> status,message 반환으로 변경

--- src/main/java/com/sparta/demo/service/InvitationService.java
+++ src/main/java/com/sparta/demo/service/InvitationService.java
@@ -1,20 +1,23 @@
 package com.sparta.demo.service;
 
 
-import com.sparta.demo.dto.ApiResponseDto;
-import com.sparta.demo.dto.BoardResponseDto;
+import com.sparta.demo.dto.response.ApiResponseDto;
+import com.sparta.demo.dto.response.BoardResponseDto;
 import com.sparta.demo.entity.Board;
 import com.sparta.demo.entity.User;
 import com.sparta.demo.entity.UserBoardRelation;
 import com.sparta.demo.repository.BoardRepository;
 import com.sparta.demo.repository.UserBoardRelationRepository;
 import com.sparta.demo.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.juli.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class InvitationService {
     @Autowired
     private UserRepository userRepository;
@@ -32,7 +35,7 @@ public class InvitationService {
         // 해당되는 userId, BoardId가 존재하지 않거나, 이미 초대된 사용자임을 검증하는 로직
         UserBoardRelation existingRelation = userBoardRelationRepository.findByUserIdAndBoardId(userId, boardId);
         if (existingRelation != null && "INVITED".equals(existingRelation.getInvitationStatus())) {
-            throw new IllegalArgumentException("이미 초대된 사용자입니다.");
+            return ResponseEntity.status(400).body(new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), "이미 초대된 사용자입니다."));
         }
 
         // 초대 로직 구현
@@ -52,7 +55,7 @@ public class InvitationService {
                 .orElseThrow(() -> new IllegalArgumentException("초대장을 찾을 수 없습니다."));
 
         if (!"INVITED".equals(relation.getInvitationStatus())) {
-            throw new IllegalStateException("초대를 받지 않았거나, 이미 요청 수락이 되었습니다.");
+            return ResponseEntity.status(400).body(new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), "이미 초대를 수락한 사용자입니다."));
         }
 
         relation.setInvitationStatus("ACCEPTED");
