29fa702ac3fb5122b8eed4d6626081385f49220a
insu <insubono@gmail.com>
2023-08-10 17:33:12 +0900

    <Controller> 보드 생성, 보드 수정, 상세 보드 삭제 반환형 string, 반환데이터 "index"로 수정

--- src/main/java/com/sparta/demo/controller/BoardController.java
+++ src/main/java/com/sparta/demo/controller/BoardController.java
@@ -1,11 +1,10 @@
 package com.sparta.demo.controller;
 
 import com.sparta.demo.dto.response.ApiResponseDto;
-import com.sparta.demo.dto.reuqest.BoardRequestDto;
 import com.sparta.demo.dto.response.BoardResponseDto;
+import com.sparta.demo.dto.reuqest.BoardRequestDto;
 import com.sparta.demo.security.UserDetailsImpl;
 import com.sparta.demo.service.BoardService;
-import com.sparta.demo.service.InvitationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,15 +14,15 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
-@RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
 
     @PostMapping("/Board") // 보드 생성
-    public BoardResponseDto createBoard(@RequestBody BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return boardService.createBoard(requestDto, userDetails.getUser());
+    public String createBoard(@ModelAttribute BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        boardService.createBoard(requestDto, userDetails.getUser());
+        return "index";
     }
 
 //    @GetMapping("/Board")
@@ -31,24 +30,28 @@ public class BoardController {
 //        return boardService.getBoards();
 //    }
 
+    @ResponseBody
     @GetMapping("/allBoards") // 초대받은 보드 + 본인이 생성한 보드 전체 조회
     public ResponseEntity<ApiResponseDto> getAllUserBoards(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return boardService.getAllUserBoards(userDetails.getUser());
     }
 
+    @ResponseBody
     @GetMapping("/Board/{id}") // 상세 보드 조회
     public BoardResponseDto lookupBoard(@PathVariable Long id) {
         return boardService.lookupBoard(id);
     }
 
     @PutMapping("/Board/{id}") // 보드 수정
-    public ResponseEntity<ApiResponseDto> updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return boardService.updateBoard(id, requestDto, userDetails.getUser());
+    public String updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+         boardService.updateBoard(id, requestDto, userDetails.getUser());
+         return "index";
     }
 
-    @DeleteMapping("/Board/{id}") // 상세 게시글 삭제
-    public ResponseEntity<ApiResponseDto> deleteBoard(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return boardService.deleteBoard(id, userDetails.getUser());
+    @DeleteMapping("/Board/{id}") // 상세 보드 삭제
+    public String deleteBoard(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+         boardService.deleteBoard(id, userDetails.getUser());
+         return "index";
     }
 }
 
