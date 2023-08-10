1e0191c25f54bf1872bd1113df6e3a9fa2771708
kim jun young <wnsdud4949@naver.com>
2023-08-10 11:35:50 +0900

    HomeController 추가

---
+++ src/main/java/com/sparta/demo/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.sparta.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "index";
+    }
+
+    @GetMapping("/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup-page")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @GetMapping("/card-page")
+    public String cardPage() {
+        return "card";
+    }
+}
