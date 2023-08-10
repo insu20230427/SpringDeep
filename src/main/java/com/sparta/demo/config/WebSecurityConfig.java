8fdbcde93bef380aec4f40397419108656291cfc
insu <insubono@gmail.com>
2023-08-10 10:14:53 +0900

    <Config> 인증처리 url 추가 ( /api/** , /invitation/** )

--- src/main/java/com/sparta/demo/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/demo/config/WebSecurityConfig.java
@@ -61,6 +61,8 @@ public class WebSecurityConfig {
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/auth/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/invitation/**").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
         http.formLogin((formLogin) ->
