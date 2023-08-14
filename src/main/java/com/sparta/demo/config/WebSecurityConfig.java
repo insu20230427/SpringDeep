30a552040d41e589bb31a875cc915f8fb66abc50
kim jun young <wnsdud4949@naver.com>
2023-08-14 12:00:04 +0900

    찐 마무리

--- src/main/java/com/sparta/demo/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/demo/config/WebSecurityConfig.java
@@ -62,6 +62,7 @@ public class WebSecurityConfig {
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/auth/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/signup-page").permitAll()
                         .requestMatchers("/invitation/**").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
